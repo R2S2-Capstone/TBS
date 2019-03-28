1824fb1fe562b84ae92750585b4e64484f11e1f1
reecerose <reecerose18@gmail.com>
2019-03-28 15:55:01 -0400

    Added all required links to navigation

--- TBS.Website/src/App.vue
+++ TBS.Website/src/App.vue
@@ -1,20 +1,30 @@
 <template>
 	<div>
-		<Header/>
+		<TheHeader/>
 		<router-view/>
 	</div>
 </template>
 
 <script>
-import Header from '@/components/navigation/Header.vue'
+import TheHeader from '@/components/navigation/Header.vue'
 export default {
 	name: 'app',
 	components: {
-		Header
+		TheHeader
 	}
 }
 </script>
 
 
 <style lang="scss">
+@import "@/assets/scss/global.scss";
+* {
+    padding: 0;
+    margin: 0;
+    font-family: 'Montserrat', sans-serif;
+    color: color(mainText);
+}
+.bg-blue {
+	background-color: color(colorPrimary);
+}
 </style>
--- TBS.Website/src/components/navigation/Header.vue
+++ TBS.Website/src/components/navigation/Header.vue
@@ -1,19 +1,35 @@
 <template>
 	<nav class="navbar navbar-expand-lg navbar-light bg-blue">
-			<router-link :to="{ name: 'home' }" class="navbar-brand">Transportation Bidding System</router-link>
+			<router-link :to="{ name: 'home' }" class="navbar-brand text-white">Transportation Bidding System</router-link>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigationBar" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse text-center" id="navigationBar">
 				<ul class="navbar-nav mr-auto">
 					<li class="nav-item">
-						<router-link :to="{ name: 'home' }" class="btn">Home</router-link>
+						<router-link :to="{ name: 'home' }" class="btn text-white">Home</router-link>
+					</li>
+					<!-- TODO: User must be logged in -->
+					<li class="nav-item">
+						<router-link :to="{ name: 'createPost' }" class="btn text-white">Create a Post</router-link>
+					</li>
+					<!-- TODO: If user is a dealer show TSP posts instead of dealerPosts -->
+					<li class="nav-item">
+						<router-link :to="{ name: 'dealerPosts' }" class="btn text-white">View Posts</router-link>
 					</li>
 				</ul>
 				<ul class="navbar-nav ml-auto">
 					<li class="nav-item">
-						<router-link :to="{ name: 'login' }" class="btn"><i class="fas fa-sign-in-alt"></i> Login</router-link>
+						<router-link :to="{ name: 'managePosts' }" class="btn text-white">Manage Posts</router-link>
 					</li>
+					<!-- TODO: User must not be logged in -->
+					<li class="nav-item">
+						<router-link :to="{ name: 'login' }" class="btn text-white"><i class="fas fa-sign-in-alt"></i> Login</router-link>
+					</li>
+					<!-- TODO: User must be logged in -->
+                    <li class="nav-item">
+                        <span class="btn text-white" @click="logout"><i class="fas fa-sign-out-alt"></i> Logout</span>
+                    </li>
 				</ul>
 		</div>
 	</nav>
