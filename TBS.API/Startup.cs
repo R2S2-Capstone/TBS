94007d78f25e7b694623f5c967c85a3ac00ec61d
reecerose <reecerose18@gmail.com>
2019-06-04 19:03:49 -0400

    Added swagger

--- TBS.API/Startup.cs
+++ TBS.API/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace TBS.API
 {
@@ -25,6 +26,11 @@ namespace TBS.API
 
             services.AddCors();
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Transportation Bidding System", Version = "v1" });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -42,6 +48,13 @@ namespace TBS.API
             }
 
             app.UseAuthentication();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Transportation Bidding System API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseCors(builder =>
             {
