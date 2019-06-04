444d52144a4056209d3deecc8ab437358d6d5694
reecerose <reecerose18@gmail.com>
2019-06-04 19:00:45 -0400

    Added API versioning

---
+++ TBS.API/Controllers/v1/AuthenticationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TBS.API.Controllers.v1
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+    }
+}
