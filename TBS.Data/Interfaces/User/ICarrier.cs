121eb6d4685e0ce139adf78147cea41c3b22f442
reecerose <reecerose18@gmail.com>
2019-06-25 20:50:01 -0400

    Finished basics of Shipper and Carrier interface

--- TBS.Data/Interfaces/User/ICarrier.cs
+++ TBS.Data/Interfaces/User/ICarrier.cs
@@ -1,9 +1,18 @@
-﻿using TBS.Data.Models.User;
+﻿using System.Collections.Generic;
+using TBS.Data.Models.User;
 
 namespace TBS.Data.Interfaces.User
 {
     public interface ICarrier
     {
         Carrier Get(int id);
+
+        IEnumerable<Carrier> GetAll();
+
+        //Get all posts
+
+        //Get all bids
+
+
     }
 }
--- TBS.Data/Interfaces/User/IShipper.cs
+++ TBS.Data/Interfaces/User/IShipper.cs
@@ -1,6 +1,18 @@
-﻿namespace TBS.Data.Interfaces.User
+﻿using System.Collections.Generic;
+using TBS.Data.Models.User;
+
+namespace TBS.Data.Interfaces.User
 {
-    public class IShipper
+    public interface IShipper
     {
+        Shipper Get(int id);
+
+        IEnumerable<Shipper> GetAll();
+
+        //Get all posts
+
+        //Get all bids
+
+
     }
 }
