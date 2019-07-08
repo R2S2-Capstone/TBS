74826dff039a95163e0a87495d67d960add72a5e
reecerose <reecerose18@gmail.com>
2019-07-08 18:55:05 -0400

    Added interface for carrier post

---
+++ TBS.Data/Interfaces/Post/ICarrierPostService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TBS.Data.Models.Post;
+
+namespace TBS.Data.Interfaces.Post
+{
+    public interface ICarrierPostService
+    {
+        Task<IEnumerable<CarrierPost>> GetAllPostsAsync();
+
+        Task<IEnumerable<CarrierPost>> GetAllActivePostsAsync();
+
+        Task<CarrierPost> GetPostByIdAsync(int id);
+
+        Task<bool> CreatePostAsync(CarrierPost post);
+
+        Task<bool> UpdatePostAsync(CarrierPost post);
+
+        Task<bool> RemovePostAsync(int id);
+    }
+}
