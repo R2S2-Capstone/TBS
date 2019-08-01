98a1fdde828e0a015041c656668e0c95835b10cb
reecerose <reecerose18@gmail.com>
2019-08-01 17:35:06 -0400

    Updated SQL scripts

--- SQL/Copy DEV users into PROD.sql
+++ SQL/Copy DEV users into PROD.sql
@@ -1,5 +1,5 @@
 INSERT INTO `reeceros_tbs`.`Address`(`Id`, `AddressLine`, `City`, `Province`, `Country`, `PostalCode`) SELECT `Id`, `AddressLine`, `City`, `Province`, `Country`, `PostalCode` FROM `reeceros_tbs-dev`.`Address`;
-INSERT INTO `reeceros_tbs`.`Carriers`(`Id`, `UserFirebaseId`, `Name`, `Email`, `CompanyId`) SELECT `Id`, `UserFirebaseId`, `Name`, `Email`, `CompanyId` FROM `reeceros_tbs-dev`.`Carriers`;
+INSERT INTO `reeceros_tbs`.`Carriers`(`Id`, `UserFirebaseId`, `Name`, `Email`, `CompanyId`, `DriversLicense`, `VehicleId`) SELECT `Id`, `UserFirebaseId`, `Name`, `Email`, `CompanyId`, `DriversLicense`, `VehicleId` FROM `reeceros_tbs-dev`.`Carriers`;
 INSERT INTO `reeceros_tbs`.`Company`(`Id`, `Name`, `AddressId`, `ContactId`) SELECT `Id`, `Name`, `AddressId`, `ContactId` FROM `reeceros_tbs-dev`.`Company`;
-INSERT INTO `reeceros_tbs`.`Contact`(`Id`, `Name`, `PhoneNumber`) SELECT `Id`, `Name`, `PhoneNumber` FROM `reeceros_tbs-dev`.`Contact`;
-INSERT INTO `reeceros_tbs`.`Shippers`(`Id`, `Name`, `UserFirebaseId`, `Email`, `CompanyId`) SELECT `Id`, `Name`, `UserFirebaseId`, `Email`, `CompanyId` FROM `reeceros_tbs-dev`.`Shippers`;
+INSERT INTO `reeceros_tbs`.`Contact`(`Id`, `Name`, `PhoneNumber`, `Email`) SELECT `Id`, `Name`, `PhoneNumber`, `Email` FROM `reeceros_tbs-dev`.`Contact`;
+INSERT INTO `reeceros_tbs`.`Shippers`(`Id`, `Name`, `UserFirebaseId`, `Email`, `CompanyId`, `RIN`, `DealerNumber`) SELECT `Id`, `Name`, `UserFirebaseId`, `Email`, `CompanyId`, `RIN`, `DealerNumber` FROM `reeceros_tbs-dev`.`Shippers`;
