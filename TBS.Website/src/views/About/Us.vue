c5e5781350a1be071655f841680a455c8285e855
Robert Middlebrook <Giantorange@hotmail.com>
2019-09-17 00:55:42 -0400

    small update

--- TBS.Website/src/views/About/Us.vue
+++ TBS.Website/src/views/About/Us.vue
@@ -7,10 +7,10 @@
         <hr>
         <p>This is a website primarily designed to fulfill a hole in the shipping service industry specifically around shipping cars. Currently, there is not anything that 
           allows for effective shipping of small amounts of cars for dealerships. In response, we've created this website to help anyone interested!
-         <br> 
+         <br> <br>
           We are a group of College students completing a Capstone project for a Solutions Architect. This is an independent project with no ties to any businesses nor have
           we even incorporated.
-        <br>
+        <br><br>
           If you wish to contact us, Please send us an email at placeholder@gmail.com and we'll get back to you as soon as possible. 
         </p>
       </div>
